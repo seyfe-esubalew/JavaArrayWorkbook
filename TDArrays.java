dd8190e7d47c658cfb58712d0b91df214df25265
Seyfe Esubalew <83219979+seyfe-esubalew@users.noreply.github.com>
2024-12-31 20:51:55 +0300

    Update TDArrays.java

--- TDArrays.java
+++ TDArrays.java
@@ -4,7 +4,8 @@ public class TDArrays {
         int [][] ints ={{75,65,50,40},{55,78,52,63},{66,76,85,96}};
 
         for(int i=0; i<ints.length;i++){
-            for(int j=0;j<ints[i].length;j++){
+            for(int j=0;j<ints[i].length;j++){ 
+                System.out.print("");
                 System.out.print(ints[i][j]+" ");
             }
             System.out.println(" ");
